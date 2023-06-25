e92a4d56aa50fef431505de7008a30d122210a4d
EvanZhe <wjz990619@163.com>
2023-06-25 21:11:58 +0800

    update

--- js/loadScript.js
+++ js/loadScript.js
@@ -1,3 +1,4 @@
+//动态脚本
 function loadScript(url) {
     if(typeof url == 'string' && url != ''){
         let script = document.createElement('script');
@@ -7,6 +8,12 @@ function loadScript(url) {
         return undefined
     }
 }
+//动态样式
 function loadStyle(url) {
-    
+    let link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    link.href = url
+    let head = document.getElementsByTagName('head')[0]
+    head.appendChild(link) 
 }
